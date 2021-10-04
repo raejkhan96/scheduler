d0485bb8b57d48c8293dbe796bb9781112b0fd1f
Rae <raekhan18@gmail.com>
2021-10-04 19:17:15 -0400

    A selector was used to create a function that will return an array of appointments for the given day.

---
+++ src/helpers/selectors.js
@@ -0,0 +1,22 @@
+// function selectUserByName(state, name) {
+//   const filteredNames = state.users.filter(user => user.name === name);
+//   return filteredNames;
+// }
+
+export function getAppointmentsForDay(state, day) {
+ 
+  const daysObj = state.days.filter(days => days.name === day);
+  // console.log(daysObj);
+  const apptObjectsArray = [];
+  if (daysObj.length === 0) {
+    return apptObjectsArray; 
+  }
+  else {
+    for (let i = 0; i < daysObj[0].appointments.length; i++) {
+      apptObjectsArray.push(state.appointments[daysObj[0].appointments[i]])
+    }
+    // console.log(apptObjectsArray);
+    return (apptObjectsArray);
+  }
+}
+  
