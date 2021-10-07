be99700ba1e338ee433e9c13973c93d765e94f98
Rae <raekhan18@gmail.com>
2021-10-07 12:36:19 -0400

    This commit involvesa refactoring of the Appointment component. The hook previously created is now able to be used. The front end now contains functionality to add and cancel for an appointment.

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -4,13 +4,14 @@ import React from "react";
 import "components/Appointment/styles.scss"
 
 export default function onAdd(props) {
-   
+  console.log('ONADD ', props)
   return ( 
     <main className="appointment__add">
       <img 
         className="appointment__add-button"
         src="images/add.png"
         alt="Add"
+        // onClick = 
         onClick={props.onAdd}
       />
     </main>
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,16 +7,63 @@ import Show from "components/Appointment/Show.js"
 // // Empty has function onAdd
 import Empty from "components/Appointment/Empty.js"
 import "components/Appointment/styles.scss"
+import Form from "components/Appointment/Form.js"
+import useVisualMode from "hooks/useVisualMode";
 
-
+// add within function?
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
   return ( 
     <article className="appointment">
       <Header time={props.time}/>
-      {props.interview ? (<Show student={props.interview.student} interviewer={props.interview.interviewer.name} />) : (<Empty />) }
+      {/* {props.interview ? (<Show student={props.interview.student} interviewer={props.interview.interviewer.name} />) : (<Empty />) } */}
+      {/* SHOW MODE displays booked interviews */}
+      {mode === SHOW && props.interview && (
+      <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+      />
+      )} 
+      {/*EMPTY MODE fills the calendar with the onAdd options */}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {/* Without the CREATE mode, there is nothing to transition to and the section of the calendar disappears when clicked */}
+      {mode === CREATE && <Form interviewers = {[]} onCancel = { back }/> }
     </article>
   );
 
 }
+
+// export default function Appointment(props) {
+//   const EMPTY = "EMPTY";
+//   const SHOW = "SHOW";
+//   const CREATE = "CREATE"
+//   const { mode, transition, back } = useVisualMode(
+//     props.interview ? SHOW : EMPTY
+//   );
+//   return (
+//     <article className="appointment">
+//       <Header time={props.time} />
+//       {/* {props.interview ? (
+//         <Show
+//           student={props.interview.student}
+//           interviewer={props.interview.interviewer.name}/>) : (<Empty />)} */}
+//        {mode === EMPTY && <Empty onAdd={() => transition(CREATE)}  />}
+//        {mode === SHOW &&  (
+//         <Show
+//           student={props.interview.student}
+//           interviewer={props.interview.interviewer}
+//         />
+//       )}
+
+//       {mode=== CREATE && (<Form interviewers = [] />)}
+//     </article>
+//   );
+// } 
